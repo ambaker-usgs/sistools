3ce1eea11759335cf1a55228a55e81f51e4f45d3
Adam Baker <ambaker@usgs.gov>
2015-07-16 12:57:53 -0600

    Minor fix
    Minor fix, made UTCDateTime a string

--- genstationxml.py
+++ genstationxml.py
@@ -212,8 +212,8 @@ def processStationComments(blockettes):
 		if blockette.id == 51:
 			appendToFile(3, ['<fsx:Comment>'])
 			appendToFile(4, ['<fsx:Value>' + fetchComment(dictB031, blockette.comment_code_key)[0] + '</fsx:Value>'])
-			appendToFile(4, ['<fsx:BeginEffectiveTime>' + blockette.beginning_effective_time +  '</fsx:BeginEffectiveTime>'])
-			appendToFile(4, ['<fsx:EndEffectiveTime>' + blockette.end_effective_time + '</fsx:EndEffectiveTime>'])
+			appendToFile(4, ['<fsx:BeginEffectiveTime>' + str(blockette.beginning_effective_time) +  '</fsx:BeginEffectiveTime>'])
+			appendToFile(4, ['<fsx:EndEffectiveTime>' + str(blockette.end_effective_time) + '</fsx:EndEffectiveTime>'])
 			appendToFile(4, ['<fsx:Author>'])
 			appendToFile(5, ['<fsx:Name>' + 'USGS ASL RDSEED' + '</fsx:Name>'])
 			appendToFile(4, ['</fsx:Author>'])
