dfed3edc4c65aedba9cdd7d7d9c3b577867c1f2f
Adam Baker <ambaker@usgs.gov>
2015-07-17 12:16:39 -0600

    Potential fix for station dataless
    Testing a potential fix for station dataless

--- datalesstools.py
+++ datalesstools.py
@@ -8,33 +8,46 @@
 ###################################################################################################
 
 #imports
+import os
 import sys
+from aslparser import Parser as aslParser
 from obspy.xseed import Parser
 
 #variables
-datalessPath = '/APPS/metadata/SEED/'
+netDatalessPath = '/APPS/metadata/SEED/'
+staDatalessPath = '/dcc/metadata/dataless/'
 
 #functions
 def getDataless(netsta):
 	#the function that returns the raw dataless
 	net = netsta[:2].upper()
-	parsedDataless = Parser(datalessPath + net + '.dataless')
+	parsedDataless = Parser(netDatalessPath + net + '.dataless')
 	return parsedDataless
 
 def getStationDataless(netsta):
 	#the function that returns the dataless for a given station
 	net = netsta[:2].upper()
-	parsedDataless = Parser(datalessPath + net + '.dataless')
-	if len(netsta) > 2:
-		sta = netsta[2:].upper()
+	sta = netsta[2:].upper()
+	netsta = '_'.join([net,sta])
+	if os.path.exists(staDatalessPath + 'DATALESS.' + netsta + '.seed'):
+		station = []
+		parsedDataless = aslParser(staDatalessPath + 'DATALESS.' + netsta + '.seed')
 		for station in parsedDataless.stations:
-			for blockette in station:
-				if blockette.id == 50:
-					if blockette.station_call_letters == sta:
-						return station
+			if type(station) is list:
+				station.extend(station[0])
+		return station
+	else:
+		parsedDataless = Parser(netDatalessPath + net + '.dataless')
+		if len(netsta) > 2:
+			sta = netsta[2:].upper()
+			for station in parsedDataless.stations:
+				for blockette in station:
+					if blockette.id == 50:
+						if blockette.station_call_letters == sta:
+							return station
 
 def getNetworkDataless(netsta):
 	#the function that returns the dataless for a given network
 	net = netsta[:2].upper()
-	parsedDataless = Parser(datalessPath + net + '.dataless')
+	parsedDataless = Parser(netDatalessPath + net + '.dataless')
 	return parsedDataless.stations
